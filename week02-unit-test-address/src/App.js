378dd46d336d786fd05e2d81a4511cab1045479b
bcuser <chad.allred@bellevuecollege.edu>
2017-04-22 14:55:40 -0700

    Push Working Code before 3pm changes

--- week02-unit-test-address/src/App.js
+++ week02-unit-test-address/src/App.js
@@ -1,56 +1,34 @@
 import React, {Component} from 'react';
 import './css/App.css';
-import ElfHeader from './components/elfheader.js';
-import Address from './components/Address'
 import './css/index.css';
+import ElfHeader from './components/elfheader';
+import Address from './components/address';
 
-class Address extends Component {
+
+class App extends Component {
     constructor() {
         super();
-        const unknown = 'unknown';
         this.state = {
             nine: '0',
-            firstName: unknown,
-            lastName: unknown,
-            streetAddress: unknown,
-            city: unknown,
-            state: unknown,
-            zipCode: unknown
+
         }
     }
+        render() {
+            return (
+                <div className="App">
+                    <ElfHeader/>
+                    <p className="App-intro">Nine: {this.state.nine}</p>
+                    <Address/>
+                    <button className='elf' id='nine' onClick={this.getNine}>Get Nine</button>
+                </div>
+            );
+        }
 
-    render() {
-        return (
-            <div className="App">
-                <Address/>
-                <ElfHeader/>
-                <p className="App-intro">Nine: {this.state.nine}</p>
-                <p className="App-intro">firstName: {this.state.firstName}</p>
-                <p className="App-intro">lastName: {this.state.lastName}</p>
-                <p className="App-intro">streetAddress: {this.state.streetAddress}</p>
-                <p className="App-intro">city: {this.state.city}</p>
-                <p className="App-intro">state: {this.state.state}</p>
-                <p className="App-intro">zipCode: {this.state.zipCode}</p>
-                <button className='elf' id='nine' onClick={this.getNine}>Get Nine</button>
-                <button className='elf' id='address' onClick={this.setAddress}>Set Address</button>
-            </div>
-        );
+        getNine = () => {
+            this.setState({nine: '9'})
+        };
+
     }
 
-    getNine = () => {
-        this.setState({nine: '9'})
-    };
 
-    setAddress = () => {
-        this.setState({
-            firstName: 'Patty',
-            lastName: 'Murray',
-            streetAddress: '915 2nd Ave #2988',
-            city: 'Seattle',
-            state: 'Washington',
-            zipCode: '98174'
-        })
-    };
-}
-
-export default Address;
+export default App;
---
+++ week02-unit-test-address/src/components/address.js
@@ -0,0 +1,50 @@
+/**
+ * Created by bcuser on 4/22/17.
+ */
+import React, {Component} from 'react';
+
+import addresses from './addresslist';
+
+class Address extends Component {
+    constructor() {
+        super();
+        const unknown = 'unknown';
+        this.state = {
+            firstName: unknown,
+            lastName: unknown,
+            streetAddress: unknown,
+            city: unknown,
+            State: unknown,
+            zipCode: unknown
+        }
+    }
+
+    render() {
+            <div>
+        return (
+            <div className="App">
+                <p className="App-intro">firstName: {this.state.firstName}</p>
+                <p className="App-intro">lastName: {this.state.lastName}</p>
+                <p className="App-intro">streetAddress: {this.state.streetAddress}</p>
+                <p className="App-intro">city: {this.state.city}</p>
+                <p className="App-intro">state: {this.state.State}</p>
+                <p className="App-intro">zipCode: {this.state.zipCode}</p>
+                <button className='elf' id='address' onClick={this.setAddress}>Set Address</button>
+            </div>
+        );
+    }
+
+    setAddress = () => {
+        this.setState({
+            firstName: 'Patty',
+            lastName: 'Murray',
+            streetAddress: '915 2nd Ave #2988',
+            city: 'Seattle',
+            State: 'Washington',
+            zipCode: '98174'
+        })
+    };
+}
+
+
+export default Address;
