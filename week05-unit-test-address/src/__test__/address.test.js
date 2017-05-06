006390bdca5a41ed8561ac8d5c51392b17a55bd3
bcuser <chad.allred@bellevuecollege.edu>
2017-05-05 18:04:12 -0700

    Commit for React Address Router Assignement

--- week05-unit-test-address/src/__test__/address.test.js
+++ week05-unit-test-address/src/__test__/address.test.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import {mount} from 'enzyme';
-import Address from '../components/address';
+import Address from '../components/address'
 import Addresses from '../components/address-list';
 import '../css/index.css';
 import ElfTestDebug from '../elftestdebug';
@@ -25,41 +25,41 @@ describe('Address Test Suite', function () {
     it('renders button Set Address click message: firstName', () => {
         const wrapper = mount(<Address />);
         const AddressSign = <p className="App-intro">firstName: Patty</p>;
-        wrapper.find('button#getAddress').simulate('click');
+        wrapper.find('button#setAddress').simulate('click');
         expect(wrapper.contains(AddressSign)).toEqual(true);
     });
 
     it('renders button Set Address click message: lastName', () => {
         const wrapper = mount(<Address />);
         const AddressSign = <p className="App-intro">lastName: Murray</p>;
-        wrapper.find('button#getAddress').simulate('click');
+        wrapper.find('button#setAddress').simulate('click');
         expect(wrapper.contains(AddressSign)).toEqual(true);
     });
     it('renders button Set Address click message: streetAddress', () => {
         const wrapper = mount(<Address />);
         const AddressSign = <p className="App-intro">streetAddress: 915 2nd Ave #2988</p>;
-        wrapper.find('button#getAddress').simulate('click');
+        wrapper.find('button#setAddress').simulate('click');
         expect(wrapper.contains(AddressSign)).toEqual(true);
     });
 
     it('renders button Set Address click message: city', () => {
         const wrapper = mount(<Address />);
         const AddressSign = <p className="App-intro">city: Seattle</p>;
-        wrapper.find('button#getAddress').simulate('click');
+        wrapper.find('button#setAddress').simulate('click');
         expect(wrapper.contains(AddressSign)).toEqual(true);
     });
 
     it('renders button Set Address click message: state', () => {
         const wrapper = mount(<Address />);
         const AddressSign = <p className="App-intro">state: Washington</p>;
-        wrapper.find('button#getAddress').simulate('click');
+        wrapper.find('button#setAddress').simulate('click');
         expect(wrapper.contains(AddressSign)).toEqual(true);
     });
 
     it('renders button Set Address click message: zipCode', () => {
         const wrapper = mount(<Address />);
         const AddressSign = <p className="App-intro">zipCode: 98174</p>;
-        wrapper.find('button#getAddress').simulate('click');
+        wrapper.find('button#setAddress').simulate('click');
         expect(wrapper.contains(AddressSign)).toEqual(true);
     });
 
