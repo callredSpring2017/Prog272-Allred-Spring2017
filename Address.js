741d8b85a360e43f367a4c1f28937b4dbfdd2c84
callredSpring2017 <chad.allred@bellevuecollege.edu>
2017-06-10 16:13:12 -0700

    Adding sourcefiles for ReactNativeBasics assignement

---
+++ Address.js
@@ -0,0 +1,79 @@
+/**
+ * by Charlie Calvert
+ */
+
+import React, {Component} from 'react';
+import {
+    Alert,
+    Button,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+export default class Address extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foo: 'expo try state: ',
+            firstName: 'Click ',
+            lastName : 'GET DATA'
+        };
+        this.collection = [];
+        this.fetchAddress = this.fetchAddress.bind(this);
+    }
+
+    fetchAddress(event) {
+        this.setState({foo:'fetcher king'});
+        const that = this;
+        const ip = '168.156.47.95';
+        fetch('http://' + ip + ':30025/all-data')
+            .then((response) => response.json())
+            .then(function(result) {
+                this.collection = result.allData;
+                that.setState({
+                    foo: 'qux success: ',
+                    firstName: this.collection[0].firstName + ' ',
+                    lastName: this.collection[0].lastName
+                });
+            }).catch(function(ex) {
+            that.setState({foo: 'qux error'});
+        });
+        if (event) {
+            event.preventDefault();
+        }
+    };
+
+    render() {
+        console.log('tst');
+
+        return (
+            <View>
+                <Text style={styles.instructions}>
+                    {this.state.foo}
+                </Text>
+                <Text style={styles.instructions}>
+                    {this.state.firstName}
+                    {this.state.lastName}
+                </Text>
+                <Button
+                    onPress={this.fetchAddress}
+                    title="Get Data"
+                    color="#841584"
+                    accessibilityLabel="Learn more about this purple button"
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5,
+    },
+});
+
+AppRegistry.registerComponent('Address', () => Address);
